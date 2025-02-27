03a45455776e8c7b6c399c9e28771d14cc96cdb7
Dmitry <165063315+MrJonson75@users.noreply.github.com>
2025-02-27 19:48:53 +0300

    Add files via upload

--- 10 Дополнительно/10_6 Конструкция match_case со словарями и множествами/tasks 10_6.py
+++ 10 Дополнительно/10_6 Конструкция match_case со словарями и множествами/tasks 10_6.py
@@ -27,6 +27,8 @@
 def parse_json(data):
     match data:
         # здесь прописывайте шаблон
+        case {'access': bool(access), 'data': list([data, _, _, _])}:
+            return access, data
 
         case {'id': ids, 'data': [_, {'login': login}, _, _]}:
             return ids, login
@@ -34,3 +36,48 @@ def parse_json(data):
     return None
 
 json_data = {'id': 2, 'access': False, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+
+
+print(parse_json(json_data))
+
+
+
+
+
+'''
+        Подвиг 5. Имеется следующий фрагмент программы с функцией parse_json и словарем json_data:
+
+        def parse_json(data):
+            match data:
+                case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+                    return ids, login
+            
+            return None
+        
+        json_data = 
+        {'id': 2, 'access': True, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+        
+        С помощью оператора match/case в функцию parse_json добавьте в самое начало шаблон для выделения 
+        значений ключей login и email с проверкой, что они являются строками и при условии, что поле 
+        access принимает значение True. 
+        Возвратите выделенные два значения в виде кортежа в формате (login, email).
+        
+        P. S. В программе нужно только дописать шаблон. Вызывать функцию не нужно.
+'''
+
+
+def parse_json(data):
+    match data:
+        # здесь прописывайте шаблон
+        case {'access': bool() as access, 'data': [_, {'login': str(login), 'email': str(email)}, _, _]} if access:
+            return login, email
+
+        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+            return ids, login
+
+    return None
+
+json_data = {'id': 2, 'access': True, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+
+
+print(parse_json(json_data))
